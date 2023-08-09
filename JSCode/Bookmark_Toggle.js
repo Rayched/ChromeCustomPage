d097b0957db0559accd9d61426fb75b663b80247
Rayched <ny2401@naver.com>
2023-08-09 20:20:18 +0900

    기존 Code에서 조건문이 과하게 있어서 early return 패턴으로 단순화 하였음

--- JSCode/Bookmark_Toggle.js
+++ JSCode/Bookmark_Toggle.js
@@ -5,19 +5,13 @@ const BookmarkClose = document.getElementById("Bookmark_close");
 const BookmarkBarToggle = () => {
     const isBookmarkBarOpen = localStorage.getItem("isBookmarkBarOpen")
     
-    if(isBookmarkBarOpen){
-        if(isBookmarkBarOpen==="open"){
-            localStorage.setItem("isBookmarkBarOpen", "close");
-            BookmarkBar.style.display = "none";
-            BookmarkOpen.style.display = "none";
-            BookmarkClose.style.display = "flex";
-        }
-        else {
-            localStorage.setItem("isBookmarkBarOpen", "open");
-            BookmarkBar.style.display="block";
-            BookmarkOpen.style.display="flex";
-            BookmarkClose.style.display="none";
-        }
+   
+    if(isBookmarkBarOpen==="close"){
+        localStorage.setItem("isBookmarkBarOpen", "open");
+        BookmarkBar.style.display="block";
+        BookmarkOpen.style.display="flex";
+        BookmarkClose.style.display="none";
+        return;
     }
     else {
         localStorage.setItem("isBookmarkBarOpen", "close");
@@ -25,7 +19,19 @@ const BookmarkBarToggle = () => {
         BookmarkOpen.style.display = "none";
         BookmarkClose.style.display = "flex";
     }
+    /*
+        먼저 작성한 코드는 if, else 조건문이 많기 때문에 코드의 가독성이 떨어져보인다.
+        early return 패턴을 적용해서 Code를 간략화한다고 했다.
+        근데 early return이 뭐지..?
+        궁금해서 구글링을 해서, 다른 개발자 분이 블로그에 정리해놓은 글을
+        잠깐 살펴봤지만..."음, 완벽하게 이해했어!!(이해하지 못함)" 상태가 되버렸다.
+        오늘 마음이 뒤숭숭해서 그런지 평소보다 피곤한 것 같아서 강의의 2/3 정도만 수행하고
+        일단 오늘은 여기까지 하도록 하자.
+        타고난 오성이 게을러서 그런지 작업 효율이 잘 안나온다.
+        오히려 3개월 전에 주 5일 일했던 때보다 떨어지는 것 같다...
+        이러면 안되는데 말이야...
+    */
 }
 
 document.getElementById("Bookmark_open_btn").addEventListener("click", BookmarkBarToggle);
-document.getElementById("Bookmark_close_btn").addEventListener("click", Boo);
+document.getElementById("Bookmark_close_btn").addEventListener("click", BookmarkBarToggle);
