796676cd9b4377eb36d5809da190b2af597b21ab
Rayched <ny2401@naver.com>
2023-08-09 20:01:55 +0900

    북마크바 열기/닫기 logic 추가

---
+++ JSCode/Bookmark_Toggle.js
@@ -0,0 +1,31 @@
+const BookmarkBar = document.getElementById("Bookmark_bar");
+const BookmarkOpen = document.getElementById("Bookmark_open");
+const BookmarkClose = document.getElementById("Bookmark_close");
+
+const BookmarkBarToggle = () => {
+    const isBookmarkBarOpen = localStorage.getItem("isBookmarkBarOpen")
+    
+    if(isBookmarkBarOpen){
+        if(isBookmarkBarOpen==="open"){
+            localStorage.setItem("isBookmarkBarOpen", "close");
+            BookmarkBar.style.display = "none";
+            BookmarkOpen.style.display = "none";
+            BookmarkClose.style.display = "flex";
+        }
+        else {
+            localStorage.setItem("isBookmarkBarOpen", "open");
+            BookmarkBar.style.display="block";
+            BookmarkOpen.style.display="flex";
+            BookmarkClose.style.display="none";
+        }
+    }
+    else {
+        localStorage.setItem("isBookmarkBarOpen", "close");
+        BookmarkBar.style.display = "none";
+        BookmarkOpen.style.display = "none";
+        BookmarkClose.style.display = "flex";
+    }
+}
+
+document.getElementById("Bookmark_open_btn").addEventListener("click", BookmarkBarToggle);
+document.getElementById("Bookmark_close_btn").addEventListener("click", BookmarkBarToggle);
