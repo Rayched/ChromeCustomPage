c6b796d7f7e074600fe74c2630b27b9a39a9e60d
Rayched <ny2401@naver.com>
2023-08-24 15:27:04 +0900

    추가한 북마크 아이템을 북마크 바에 표시하는 기능 구현 (error 발생)

--- JSCode/bookmark.js
+++ JSCode/bookmark.js
@@ -4,16 +4,15 @@ const newBookmarkForm = document.getElementById("Bookmark_item_input_form");
 
 //북마크 리스트
 let BookmarkList = [];
-if(localStorage.getItem("BookmarkList")){
-    BookmarkList = JSON.parse(localStorage.getItem("BookmarkList"));
-} else {
-    localStorage.setItem("BookmarkList", JSON.stringify(BookmarkList));
-}
+localStorage.getItem("BookmarkList") 
+    ? (BookmarkList = JSON.parse(localStorage.getItem("BookmarkList")))
+    : localStorage.setItem("BookmarkList", JSON.stringify(BookmarkList));
 
 //북마크 추가 버튼 초기 설정
 let isAddBtnClick = false;
 newBookmarkForm.style.display = "none";
 
+//북마크 아이템 추가 버튼 toggle logic
 const newBookmarkToggle = () => {
     isAddBtnClick = !isAddBtnClick;
     isAddBtnClick ? (newBookmarkForm.style.display = "block") : (newBookmarkForm.style.display = "none");
@@ -56,11 +55,55 @@ const addBookmarkItem = () => {
 }
 
 //localstorage에 저장된 북마크 정보를 북마크 바에 표시하는 logic
-const newBookmarkItemList = document.getElementById("Bookmark_list");
-//id 값이 'Bookmark_list'인 요소를 가져온 뒤, newBookmarkItemList 변수에 저장
+const BookmarkItemList = document.getElementById("Bookmark_list");
+//id 값이 'Bookmark_list'인 요소를 가져온 뒤, BookmarkItemList 변수에 저장
 
 const setBookmarkItem = (item) => {
-    console.log(item);
+    //추가한 북마크
+    const BookmarkItem = document.createElement("div");
+    BookmarkItem.classList.add("Bookmark_item");
+    BookmarkItem.id = `Bookmark_item-${item.createAt}`;
+    //id 값 = 북마크를 생성한 시간
+
+    const BookmarkInfo = document.createElement("div");
+    BookmarkInfo.classList.add("Bookmark_Info");
+    //이렇게 생성한 북마크 아이콘, 이름 요소에 'Bookmark_Info' 라는 class 값을 지정한다.
+
+    //북마크 URL을 나타내는 변수
+    const BookmarkURL = document.createElement("a");
+    BookmarkURL.classList.add("Bookmark_URL");
+    BookmarkURL.href = item.url;
+
+    const urlIcon = document.createElement("div");
+    urlIcon.classList.add("url_icon");
+
+    const UrlIconImage = document.createElement("img");
+    UrlIconImage.src=`https://www.google.com/s2/favicons?domain_url=${item.url}`;
+
+    const NameElement = document.createElement("div");
+    NameElement.classList.add("Name");
+    NameElement.textContent = item.name;
+
+    //북마크 삭제 버튼
+    const BookmarkDeleteBtn = document.createElement("div");
+    BookmarkDeleteBtn.textContent="삭제";
+    BookmarkDeleteBtn.classList.add("Delete_btn");
+
+    //BookmarkItem의 자식 요소로 BookmarkInfo, BookmarkDeleteBtn을 추가한다.
+    BookmarkItem.appendChild(BookmarkInfo);
+    BookmarkItem.appendChild(BookmarkDeleteBtn);
+
+    //BookmarkInfo의 자식 요소로 BookmarkURL 추가
+    BookmarkInfo.appendChild(BookmarkURL);
+
+    //BookmarkURL의 자식 요소로 nameElement와 UrlIconImage 추가
+    BookmarkURL.appendChild(urlIcon);
+    BookmarkURL.appendChild(NameElement);
+
+    //urlIcon의 자식 요소로 urlIconImage 추가
+    urlIcon.append(UrlIconImage);
+
+    BookmarkItemList.appendChild(BookmarkItem);
 }
 const setBookmarkList = () => {
     //localstorage에 저장된 북마크 객체들을 가져와서
